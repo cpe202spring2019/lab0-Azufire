40670f2e9678cbbb649a60fd82309a5608845215
Azufire <ssarain@calpoly.edu>
2019-04-03 10:59:30 -0700

    Fixed spelling errors.

--- planets.py
+++ planets.py
@@ -1,9 +1,9 @@
 def weight_on_planets():
     # write your code here
-    weight = int(input("What would you weigh on earth? "))
+    weight = int(input("What do you weigh on earth? "))
     mars_weight = weight * 0.38
     jupiter_weight = weight * 2.34
-    print ("\nOn Mars you would weight {0} pounds.\nOn Jupiter you would weigh {1} pounds.".format(mars_weight, jupiter_weight))
+    print ("\nOn Mars you would weigh {0} pounds.\nOn Jupiter you would weigh {1} pounds.".format(mars_weight, jupiter_weight))
    
 
 if __name__ == '__main__':
